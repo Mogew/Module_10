8bf15b4a6d59670740cb94a7d49f0e0565c6fde3
Mogew <alimovren@gmail.com>
2023-04-15 22:39:46 +0600

    complete everything

--- Module_10/Controller/ViewController.swift
+++ Module_10/Controller/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var story: UILabel!
+    @IBOutlet weak var choice2: UIButton!
+    @IBOutlet weak var choice1: UIButton!
+    
+    var model = mainModel()
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
+        choice1.titleLabel?.adjustsFontSizeToFitWidth = true
+        choice2.titleLabel?.adjustsFontSizeToFitWidth = true
     }
 
+    func updateUI() {
+        story.text = model.allStoties[model.storyIndex].title
+        choice1.setTitle(model.allStoties[model.storyIndex].choice1, for: .normal)
+        choice2.setTitle(model.allStoties[model.storyIndex].choice2, for: .normal)
+    }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        //choosePath func returns number of required story
+        model.choosePath(usersChoice: sender.currentTitle!)
+        updateUI()
+    }
 }
 
